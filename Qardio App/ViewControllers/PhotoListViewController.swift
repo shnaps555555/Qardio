956fd587ff186186bc3939d2eb9c8f092eb6cc0f
Aliaksei Kazhukhouski <shnaps555@icloud.com>
2023-08-07 16:03:13 +0300

    Add fetching and caching Images feature

--- Qardio App/ViewControllers/PhotoListViewController.swift
+++ Qardio App/ViewControllers/PhotoListViewController.swift
@@ -6,33 +6,28 @@
 //
 
 import UIKit
+import ImageCache
 
 final class PhotoListViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         NetworkService.loadPhotos(page: 1, limit: 10, searchQuery: "tele") { result in
             switch result {
             case .success(let success):
                 print(success.photos.photo.count)
                 
-//                if let photoUrl = success.photos.photo.first?.fullPath {
-//                    DispatchQueue.global().async { [weak self] in
-//                                if let data = try? Data(contentsOf: photoUrl) {
-//                                    if let image = UIImage(data: data) {
-//                                        DispatchQueue.main.async {
-//                                            image.size
-//                                        }
-//                                    }
-//                                }
-//                            }
-//                }
+                if let photoUrl = success.photos.photo.first?.fullPath {
+                    ImageCache.shared.load(url: photoUrl) { url, image in
+                        
+                    }
+                }
             case .failure(let failure):
                 print(failure.message)
             }
         }
     }
     
-
+    
 }
